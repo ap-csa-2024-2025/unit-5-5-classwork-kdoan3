6ee078c89308abd81e51736c80913dd83a456149
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-12-11 19:03:52 +0000

    fdafdsa

--- Dog.java
+++ Dog.java
@@ -1,4 +1,36 @@
 public class Dog
 {
+    private String breed;
+    private String color;
+    private double size;
 
+    public void getBreed(String iBreed)
+    {
+       return breed;
+    }
+
+    public void getColor(String iColor)
+    {
+        return color;
+    }
+
+    public void double getSize(double iSize)
+    {
+        return size;
+    }
+
+    public void setBreed(String iBreed)
+    {
+       breed = iBreed;
+    }
+
+    public void setColor(String iColor)
+    {
+       color = iColor;
+    }
+
+public void setSize(double iSize)
+    {
+      size = iSize;
+    }
 }
--- Main.java
+++ Main.java
@@ -5,5 +5,9 @@ public class Main
   public static void main(String[] args)
   {
     // Create objects and test your classes in here
+
+    Dog d = new Dog();
+    d.setBreed("terrier");
+    System.out.println(d.getBreed());
   }
 }
