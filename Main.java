a71825d19df41a27ba9fa3b6a4ea313c52a3d836
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-12-12 19:20:22 +0000

    dsdsa

--- Main.java
+++ Main.java
@@ -9,5 +9,18 @@ public class Main
     Dog d = new Dog();
     d.setBreed("terrier");
     System.out.println(d.getBreed());
+
+System.out.println("Enter the person's first name:");
+String fname = sc.nextLine();
+
+System.out.println("Enter the person's last name:");
+String lname = sc.nextLine();
+
+System.out.println("Enter the person's age:");
+int age = sc.nextInt();
+
+System.out.println("Enter the person's social security number:")
+String ss = sc.nextLine();
+
   }
 }
--- Person.java
+++ Person.java
@@ -1,4 +1,10 @@
 public class Person
 {
+    private String 
+    private String
+    private int
+    private String 
+
+
 
 }
