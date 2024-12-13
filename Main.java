b066247e244541e96bb0a3ffb268f68e4ef8ec34
kdoan3 <kbdoan103@student.hbuhsd.edu>
2024-12-13 19:18:29 +0000

    fdafdfsa

--- Main.java
+++ Main.java
@@ -22,5 +22,32 @@ int age = sc.nextInt();
 System.out.println("Enter the person's social security number:")
 String ss = sc.nextLine();
 
+
+
+Oven myOven = new Oven();
+myOven.setMaxTemp(450);
+myOven.setCurrentTemp(70);
+String input = "";
+while (!input.equals("q"))
+{
+  System.out.println(myOven); // comes from toString()
+  System.out.println("To preheat the oven enter /"p/", to turn the oven off eneter /"o/", to restart enter /"r/", .");
+  input = sc.nextLine();
+
+  if (input.equals("p"))
+  {
+    System.out.println("Enter the temperature to preheat the oven to:");
+    int temp = sc.nextInt();
+    // preheat the oven
+
+    sc.nextLine(); // consumer enter key left ehind by nextInt()
+  }
+  else if (input.equals("o"))
+  {
+    System.out.println("Turning the oven off.");
+    int temp = 0;
+  }
+}
+
   }
 }
--- Oven.java
+++ Oven.java
@@ -1,4 +1,73 @@
 public class Oven
 {
+    private int maxTemp;
+    private int currentTemp;
+
+    if (maxTemp > 500 || maxTemp < 0)
+    {
+        maxTemp = 500;
+    }
+    else
+    {
+        return maxTemp;
+    }
+
+    if (currentTemp > maxTemp)
+    {
+        currentTemp = maxTemp;
+    }
+    else if (currentTemp < 0)
+    {
+        currentTemp = 0;
+    }
+    else
+    {
+        return currentTemp;
+    }
+
+    public int getMaxTemp()
+    {
+        return maxTemp;
+    }
+
+    public int getCurrentTemp()
+    {
+        return currentTemp;
+    }
+
+    public void turnOff()
+    {
+        if (currentTemp > 0)
+        {
+            currentTemp = 0;
+        }
+    }
+
+    public boolean isOn()
+    {
+        if (currentTemp > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void preheat(int temp)
+    {
+        currentTemp = temp;
+        if (temp > maxTemp)
+        {
+            currentTemp = maxTemp
+        } 
+    }
+
+    public String toString()
+    {
+        return "New oven with a maximum temperature of " + maxTemp + " and a starting temperature of " + currentTemp;
+    }
 
 }
+
